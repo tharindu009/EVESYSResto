90759fa1474816beb7dda5f1aa01993175ed67bf
Harendra <tharindu009@hotmail.com>
2025-05-04 12:20:53 -0600

    navbar complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='bg-sky-700 h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
+            {/* Left Div */}
+            <div className='flex-[3] bg-sky-900'>
+
+            </div>
+            {/* Right Div */}
+            <div className='flex-[2] bg-sky-900'>
+
+            </div>
+            <Navbar />
+        </section>
     )
 }
 
